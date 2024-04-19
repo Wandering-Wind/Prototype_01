8acf91c824312727b5512be2086275bed7ca4768
AidenAfrica <164866194+AidenAfrica@users.noreply.github.com>
2024-04-19 13:44:36 +0200

    Importing scene
    Importing scene, board, scripts, tiles, etc.

---
+++ Assets/Scripts/TileArray.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileArray : MonoBehaviour
+{
+    int number = 50;
+    int[] numberArray = new int[50];
+ 
+
+    GameObject[] tiles;
+      public void Start()
+    {
+
+
+        for (int i = 0; i < 50; i++)
+        {
+            numberArray[i] = i + 1;
+            tiles[i] = GameObject.Find("Square (" +  i  + ")");
+        }
+        Destroy(tiles[0]);
+        tiles = GameObject.FindGameObjectsWithTag("tiles");
+        
+      
+
+     
+
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
