734211cd3b78b7f6981dd4387d887fb1ef89038d
AidenAfrica <164866194+AidenAfrica@users.noreply.github.com>
2024-04-20 16:15:25 +0200

    lists to array change

--- Assets/Scripts/List1.cs
+++ Assets/Scripts/List1.cs
@@ -4,13 +4,83 @@ using UnityEngine;
 
 public class List1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    List <string> tiles = new List <string>();
+
+   
     void Start()
     {
-        
+        tiles.Add("Tile1");
+        tiles.Add("Tile2");
+        tiles.Add("Tile3");
+        tiles.Add("Tile4");
+        tiles.Add("Tile5");
+        tiles.Add("Tile6");
+        tiles.Add("Tile7");
+        tiles.Add("Tile8");
+        tiles.Add("Tile9");
+        tiles.Add("Tile10");
+        tiles.Add("Tile11");
+        tiles.Add("Tile12");
+        tiles.Add("Tile13");
+        tiles.Add("Tile14");
+        tiles.Add("Tile15");
+        tiles.Add("Tile16");
+        tiles.Add("Tile17");
+        tiles.Add("Tile18");
+        tiles.Add("Tile19");
+        tiles.Add("Tile20");
+        tiles.Add("Tile21");
+        tiles.Add("Tile22");
+        tiles.Add("Tile23");
+        tiles.Add("Tile24");
+        tiles.Add("Tile25");
+        tiles.Add("Tile26");
+        tiles.Add("Tile27");
+        tiles.Add("Tile28");
+        tiles.Add("Tile29");
+        tiles.Add("Tile30");
+        tiles.Add("Tile31");
+        tiles.Add("Tile32");
+        tiles.Add("Tile33");
+        tiles.Add("Tile34");
+        tiles.Add("Tile35");
+        tiles.Add("Tile36");
+        tiles.Add("Tile37");
+        tiles.Add("Tile38");
+        tiles.Add("Tile39");
+        tiles.Add("Tile40");
+        tiles.Add("Tile41");
+        tiles.Add("Tile42");
+        tiles.Add("Tile43");
+        tiles.Add("Tile44");
+        tiles.Add("Tile45");
+        tiles.Add("Tile46");
+        tiles.Add("Tile47");
+        tiles.Add("Tile48");
+        tiles.Add("Tile49");
+
+       
+
+
+        for (int i = 0; i < 50; i++)
+        {
+           // tiles.Add("Tiles" + [i]);
+            Debug.Log(tiles[2]);
+        }
+            
+
+
+
+
+
+
+
+
+
+        //Debug.Log(tiles[0]);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
