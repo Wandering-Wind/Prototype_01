275f5cfbb65b8335d92d0df68fb05d6b9059ea74
Wandering-Wind <164865969+Wandering-Wind@users.noreply.github.com>
2024-05-20 12:22:18 +0200

    Gates
    Fixed the gates, still have to imprement the changes into the game scene itself. At the moment, the gate is in GateAttempt scene

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,73 +4,121 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
-{ 
-public GameObject player; // Reference to the child object to activate
-    public GameObject otherTile;
-    public GameObject ourTile;
-    
-  
+{
+    public GameObject player;// Reference to the player object to activate, if needed
+    public bool gateActive = false;
+    public Rigidbody2D boxColliderOther;
+    public Rigidbody2D boxColliderMy;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        otherTile = collision.gameObject;//OtherGameObject
-        //GameObject othergate = otherTile.transform.GetChild(0).gameObject;
-       // GameObject othergate = GameObject.Find("Gate");
-        ourTile = gameObject;
-        //Debug.Log($"Collision detected between {gameObject.name} and {collision.gameObject.name}"); // Log the names of the colliding objects
-        //Debug.Log($"Collision detected between {this.gameObject.name} and {otherTile.name}");
-       
-
-        if (collision.CompareTag("Gate"))
+        //OnTriggerEnter2D checks if the colldied object has the tag "Gate"
+        //If true, disables the 'BoxCollider2D' for both the collided object and current object 
+        
+        if ( collision.CompareTag("TriggerGateEmpty"))
         {
-            Debug.Log("Found Gate and enabling player");
-            otherTile.GetComponent<BoxCollider2D>().enabled = false;
-            ourTile.GetComponent<BoxCollider2D>().enabled = false;
-            
-            
-        }
-
-        //otherTile.GetComponent<Gates>().GetComponentInChildren<BoxCollider2D>.enabled = false;
-        //ourTile.GetComponent<Gates>().enabled = false;
-        // Check if the collided object has a specific tag (optional)
-        // If you want to activate only when colliding with certain objects, uncomment and set the tag
-        // if (collision.gameObject.CompareTag("YourTag"))
-        {
-            
-            // Activate the specific child object
-            if (player != null)
+            //Find a way to get the boxcollider in the gate
+            gateActive = true;
+            Debug.Log("Found Gate and disabling colliders");
+            foreach ( BoxCollider2D bc in collision.GetComponentsInChildren<BoxCollider2D>())
             {
-                Debug.Log($"Activating object: {player.name}"); // Log the activation action
-                player.SetActive(true);
-
+                if (bc.CompareTag("Gate"))
+                {
+                    //bc.gameObject.SetActive(false);
+                    bc.enabled = false;
+                }
             }
-            else
+
+            foreach (BoxCollider2D bc in this.GetComponentsInChildren<BoxCollider2D>())
             {
-                Debug.LogWarning("player is not assigned!"); // Warn if the object to activate is not set
-                player.SetActive(false);
+                if (bc.CompareTag("Gate"))
+                {
+                    //bc.gameObject.SetActive(false);
+                    bc.enabled = false;
+                }
             }
-            
-            
+            //this.GetComponentsInChildren<BoxCollider2D>().isTrigger = false;
+
+            //GetComponent<BoxCollider2D>().enabled = false;
         }
-    }
-
-    
-
-    private void Start()
-    {
-        if (player == null)
-        {
-            Debug.LogWarning("player is not assigned in the Inspector!"); // Initial check for the reference
-            player.SetActive(false);
-        }
-
-
+        
     }
 
     private void Update()
     {
-       otherTile.GetComponent<BoxCollider2D>().enabled = true;
-        ourTile.GetComponent<BoxCollider2D>().enabled = true;
+        //Debug.Log(gateActive);
+        //Finds all game objects with the tag "Gate" using 'GameObject.FindGameObjectsWithTag("Gate").
+        //Iterates though each gate object and enables its 'BoxCollider2D'
+        //Enables the 'BoxCollider2D' of the current game object
+        // Enable colliders for all objects tagged as "Gate"
+       //// GameObject[] gates = GameObject.FindGameObjectsWithTag("Gate");
+       // foreach (GameObject gate in gates)
+       // {
+            
+       //     if (gateActive == true)
+       //     {
+       //         gate.GetComponent<BoxCollider2D>().enabled = true;
+       //     }
+            
+       // }
+
+        // Enable the collider of the current gameObject
+        //GetComponent<BoxCollider2D>().enabled = true;
+
+        /*if (gateActive)
+           {
+            boxColliderMy.isTrigger = false;
+            boxColliderOther.isTrigger = false;
+        }
+        else
+        {
+            boxColliderMy.isTrigger = true;
+            boxColliderOther.isTrigger = true;
+        }*/
+    }
+
+
+   private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("TriggerGateEmpty") )
+        {            
+            Debug.Log("Exiting and enabling colliders");
+            gateActive = false;
+
+            foreach (BoxCollider2D bc in collision.GetComponentsInChildren<BoxCollider2D>())
+            {
+                if (bc.CompareTag("Gate"))
+                {
+                    //bc.gameObject.SetActive(true);
+                    bc.enabled = true;
+                }
+                    
+                
+            }
+
+            foreach (BoxCollider2D bc in this.GetComponentsInChildren<BoxCollider2D>())
+            {
+
+                if (bc.CompareTag("Gate"))
+                {
+                    //bc.gameObject.SetActive(true);
+                    bc.enabled = true;
+                }
+
+            }
+            //boxColliderMy.enabled = true;
+            //boxColliderOther.enabled = true;
+
+            //collision.GetComponent<BoxCollider2D>().enabled = true;
+            //GetComponent<BoxCollider2D>().enabled = true;
+        }
+
+       
+
         
     }
+
+    
 }
+
+
