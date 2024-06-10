1ad7299b81076c64110184d6923dfacf7c73eefc
AidenAfrica <164866194+AidenAfrica@users.noreply.github.com>
2024-06-10 14:17:17 +0200

    PowerUps and Traps
    Fixed the PowerUps and Traps. They spawn in max 6 at a time. Made sure that the powerups and traps do not spawn on tile 0 and tile49. The plan following this is to fix modifier 3 PowerUps

--- Assets/Scripts/ModifierManager.cs
+++ Assets/Scripts/ModifierManager.cs
@@ -19,16 +19,17 @@ public class ModifierManager : MonoBehaviour
     {
         while (true)
         {
-            if (activeModifiers.Count < 6)
+            if (activeModifiers.Count < 3)
             {
                 GameObject modifier = Instantiate(modifiers[Random.Range(0, modifiers.Length)], offScreenTransform.position, Quaternion.identity);
                 GameObject tile = tiles[Random.Range(0, tiles.Count)];
                 modifier.transform.position = tile.transform.position;
                 activeModifiers.Add(modifier);
-                StartCoroutine(RemoveModifierAfterTime(modifier, 20f));
+                StartCoroutine(RemoveModifierAfterTime(modifier, 15f));
             }
-            yield return new WaitForSeconds(5f); // Adjust spawn interval as needed
+            yield return new WaitForSeconds(4f); // Adjust spawn interval as needed
         }
+
     }
 
     IEnumerator RemoveModifierAfterTime(GameObject modifier, float time)
@@ -42,3 +43,21 @@ public class ModifierManager : MonoBehaviour
         }
     }
 }
+
+    /*finds a random number from the list of tiles (excluding the first and last. Lists start at 0 so the tiles would be 0-48).
+
+    for (int i = 0; i < num_powerups; i++)
+    {
+        //get a random number from 1 to 47
+        int r = Random.Range(1, 47);
+        //Get reference to the tile so we can work with it
+        Transform tile = boardParent.GetChild(r);
+        //Do some wizardry by Taytay
+        Instantiate(powerUp_prefab, tile.position, tile.rotation, this.transform);
+
+        this.transform.GetChild(i).gameObject.SetActive(true);
+        Debug.Log("Instantiation code"); //checking
+    }
+    */
+
+
